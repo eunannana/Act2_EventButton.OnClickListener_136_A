e96bcd7f659ca77cbc4b968d4562b5697fafbd58
eunannana <thisiseunkim93@gmail.com>
2022-03-10 21:23:49 +0700

    Add intent and passing parameter

--- app/src/main/java/com/example/act2_eventbutton_onclicklistener_136_a/MainActivity.java
+++ app/src/main/java/com/example/act2_eventbutton_onclicklistener_136_a/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.act2_eventbutton_onclicklistener_136_a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,26 +45,52 @@ public class MainActivity extends AppCompatActivity {
         name = ed_email.getText().toString();
         password = ed_password.getText().toString();
 
+        // set the right email
+        String email = "eunannana@gmail.com";
+
+        // set the right password
+        String pass = "thisisann";
+
         if (name.isEmpty() && password.isEmpty()) {
             ed_email.setError("e-mail is required!");
             ed_password.setError("password is required!");
         } else {
-            if (name.equals("eunannana@gmail.com") && password.equals("thisisann")) {
+            if (name.equals(email) && password.equals(pass)) {
                 Toast t = Toast.makeText(getApplicationContext(),
                         "Successful Login! Your e-mail : " + name + " and Your password : " + password + "", Toast.LENGTH_LONG);
                 t.show();
-            } else if (!name.equals("eunannana@gmail.com") && password.equals("thisisann")) {
+
+                Bundle b = new Bundle();
+
+                // input value from variable name with key "a" and input to the bundle
+                b.putString("a", name.trim());
+
+                //input value from variable password with key "b" and input to the bundle
+                b.putString("b", password.trim());
+
+                // make intent object move activity from mainactivity to result activity
+                Intent i = new Intent(getApplicationContext(), ResultActivity.class);
+
+                // input bundle into intent that want to send to ResultActivity
+                i.putExtras(b);
+
+                // move to ResultActivity
+                startActivity(i);
+
+            } else if (!name.equals(email) && password.equals(pass)) {
                 Toast t = Toast.makeText(getApplicationContext(),
                         "Input the correct e-mail!", Toast.LENGTH_LONG);
                 t.show();
-            } else if (name.equals("eunannana@gmail.com") && !password.equals("thisisann")) {
+            } else if (name.equals(email) && !password.equals(pass)) {
                 Toast t = Toast.makeText(getApplicationContext(),
                         "Input the correct password!", Toast.LENGTH_LONG);
                 t.show();
+
             } else {
                 Toast t = Toast.makeText(getApplicationContext(),
                         "Input the correct e-mail and password!", Toast.LENGTH_LONG);
                 t.show();
+
             }
         }
     }
